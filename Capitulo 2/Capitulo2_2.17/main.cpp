f1ccfc47583eef55fb85a708b88b1527bd1a56e9
mishelloeiza <dloeizar@miumg.edu.gt>
2024-02-27 10:30:29 -0600

    cap2

---
+++ Capitulo 2/Capitulo2_2.17/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //a) Utilizando una instrucción con un operador de inserción de flujos:
+    cout << "1 2 3 4\n";
+
+
+    //b) Utilizando una instrucción con cuatro operadores de inserción de flujos:
+    cout << "1 " << "2 " << "3 " << "4\n";
+
+
+    //c) Utilizando cuatro instrucciones:
+    cout << "1\n";
+    cout << "2\n";
+    cout << "3\n";
+    cout << "4";
+
+}
